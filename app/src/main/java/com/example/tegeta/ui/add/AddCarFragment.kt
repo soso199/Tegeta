5f0af29a90ab66a45dbe98183cea99a922d77b20
soso <tukhashvilisoso@gmail.com>
2021-07-02 18:36:53 +0400

    add car functionality

--- app/src/main/java/com/example/tegeta/ui/add/AddCarFragment.kt
+++ app/src/main/java/com/example/tegeta/ui/add/AddCarFragment.kt
@@ -1,16 +1,17 @@
 package com.example.tegeta.ui.add
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.tegeta.NavigationInterface
-import com.example.tegeta.R
 import com.example.tegeta.databinding.AddCarFragmentBinding
-import com.example.tegeta.databinding.FragmentHistoryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,7 +21,7 @@ class AddCarFragment : Fragment() {
         fun newInstance() = AddCarFragment()
     }
 
-    private  val viewModel: AddCarViewModel by viewModels()
+    private val viewModel: AddCarViewModel by viewModels()
 
     private var _binding: AddCarFragmentBinding? = null
 
@@ -38,6 +39,61 @@ class AddCarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as? NavigationInterface)?.hideAddFab()
+        viewModel.services.observe(viewLifecycleOwner) { services ->
+            val adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                services.map { it.name })
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.servicesSpinner.adapter = adapter
+
+            binding.servicesSpinner.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        viewModel.chosenService = services[position].name
+                        checkReady()
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                    }
+
+                }
+        }
+
+        binding.numberInput.addTextChangedListener(
+            object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    checkReady()
+                }
+
+            }
+        )
+
+        binding.btnEnd.setOnClickListener {
+            viewModel.addCar(binding.numberInput.text.toString())
+        }
+
+    }
+
+    fun checkReady() {
+        binding.btnEnd.isEnabled =
+            viewModel.chosenService.isNotEmpty() && binding.numberInput.text.toString().isNotEmpty()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/tegeta/ui/add/AddCarViewModel.kt
+++ app/src/main/java/com/example/tegeta/ui/add/AddCarViewModel.kt
@@ -1,25 +1,35 @@
 package com.example.tegeta.ui.add
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.tegeta.data.model.CurrentCar
+import com.example.tegeta.data.model.ServiceType
 import com.example.tegeta.data.model.Status
 import com.example.tegeta.data.repository.CurrentCarsRepository
+import com.example.tegeta.data.repository.ServicesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AddCarViewModel @Inject internal constructor(
-    private val currentCarsRepository: CurrentCarsRepository
+    private val currentCarsRepository: CurrentCarsRepository,
+    private val servicesRepository: ServicesRepository
 ) : ViewModel() {
 
-    fun addCar(number: String, serviceName: String) {
+    var chosenService: String = ""
+
+    val services: LiveData<List<ServiceType>> =
+        servicesRepository.getServices().asLiveData()
+
+    fun addCar(number: String) {
         viewModelScope.launch {
             currentCarsRepository.insertCar(
                 CurrentCar(
                     number,
-                    serviceName,
+                    chosenService,
                     Status.ADDED.value
                 )
             )
