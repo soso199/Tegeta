d32fd84a4d60546a3c2e00c05f71a2f7c3e3c315
soso <tukhashvilisoso@gmail.com>
2021-07-19 18:11:13 +0400

    added amount dialog

--- app/src/main/java/com/example/tegeta/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tegeta/ui/home/HomeFragment.kt
@@ -1,16 +1,21 @@
 package com.example.tegeta.ui.home
 
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tegeta.NavigationInterface
+import com.example.tegeta.R
 import com.example.tegeta.data.model.CurrentCar
 import com.example.tegeta.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -37,7 +42,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as? NavigationInterface)?.showAddFab()
 
-        adapter = HomeCarsAdapter(::onAddClicked)
+        adapter = HomeCarsAdapter(::showAmountDialog)
         binding.recycler.layoutManager = LinearLayoutManager(context)
         binding.recycler.adapter = adapter
 
@@ -46,8 +51,36 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun onAddClicked(car: CurrentCar) {
-        homeViewModel.endCurrentCar(car)
+    private fun showAmountDialog(car: CurrentCar) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(getString(R.string.amount_dialog_title))
+        val input = EditText(context)
+        input.setRawInputType(InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED or InputType.TYPE_NUMBER_FLAG_DECIMAL)
+
+        builder.setView(input)
+        builder.setPositiveButton(
+            getString(R.string.amount_dialog_finish)
+        ) { dialog, _ ->
+            dialog.cancel()
+            try {
+                input.text.toString().toDouble().let {
+                    if (it > 0) {
+                        car.amount = it
+                        homeViewModel.endCurrentCar(car)
+                    }
+                }
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+        }
+        builder.setNegativeButton(
+            getString(R.string.dialog_cancel)
+        ) { dialog, _ -> dialog.cancel() }
+        builder.create().apply {
+            this.setOnShowListener {
+                this.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(resources.getColor(R.color.yellow))
+            }
+        }.show()
     }
 
     override fun onResume() {
